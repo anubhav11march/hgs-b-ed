98b4637899a5ec21c90058a577e40347ad2da7a1
agrawal.abhishek <agrawal.abhishek@siemens.com>
2024-02-11 13:59:48 +0530

    files

--- src/constant/index.js
+++ src/constant/index.js
@@ -18,115 +18,212 @@ import MembersAndminutes from "../pdf/new/IQAC members and minutes of meetings (
 import MOUPDF from "../pdf/new/MOUPDF_240109_101649.pdf";
 import SEMESTERWISECALENDER from "../pdf/new/SEMESTER WISE CALENDER 21 -23 and 22-24_240109_101715.pdf";
 
-
-
-
 import NCTE2005 from "../pdf/ncte/NCTE Order 2005.pdf";
 import NCTE2015 from "../pdf/ncte/NCTE ORDERS.pdf";
-
-
+import affilation from "../pdf/ncte/3 AFFILIATION ORDERS.pdf";
+import land from "../pdf/ncte/4 CERTIFIED COPY OF LAND DOCUMENTS.pdf";
+import landuse from "../pdf/ncte/5 LAND USE CERTIFICATE.pdf";
+import buildingplan from "../pdf/ncte/6 BUILDING PLAN APPROVAL & COMPLETION CERTIFICATE.pdf";
+import non from "../pdf/ncte/7 NON-ENCUMBRANCE CERTIFICATE.JPG";
+import buildingsafety from "../pdf/ncte/8 BUILDING SAFETY & DIFFERENTLY ABLED FRIENDLY CERTIFICATE.JPG";
+import firesafety from "../pdf/ncte/9 FIRE SAFETY CERTIFICATE.pdf";
+import naacc from "../pdf/ncte/10 NAAC CERTIFICATE.jpg";
+import affidavit from "../pdf/ncte/11 AFFIDAVIT.pdf";
+import library from "../pdf/ncte/12 LIBRARY.pdf";
+import staff from "../pdf/ncte/13 STAFF DETAIL.pdf";
+import fee from "../pdf/ncte/14 FEE STRUCTURE.pdf";
+import fdr from "../pdf/ncte/15 JOINT FDR.pdf";
+import sheet from "../pdf/ncte/16 BALANCE SHEET.jpg";
+import income from "../pdf/ncte/17 INCOME & EXPENDITURE DETAILS.jpeg";
+import receipt from "../pdf/ncte/18 RECEIPT AND PAYMENT ACCOUNT FOR THE FINANCIAL YEAR.pdf";
+import madatory from "../pdf/ncte/19 Mandatory-Disclosure.docx";
 export const ResultsAcademicsData = [
-    {
-        id: 1,
-        title: "ANNUAL ACADEMIC CALENDAR",
-        document: ACADEMICCALENDAR,
-    },
-    {
-        id: 2,
-        title: "LIST OF IQAC MEETINGS",
-        document: IQACMEETINGS,
-    },
-    {
-        id: 3,
-        title: "LIST OF COMMITTEE MEMBERS",
-        document: CommitteeMembers,
-    },
-    {
-        id: 4,
-        title: "COPY OF STUDENT SATISFACTION SURVEY (SSS)",
-        document: StudentSatisfactionSurvey,
-    },
-    {
-        id: 5,
-        title: "AQAR REPORT 2017 - 2018",
-        document: AQAR2017,
-    },
-    {
-        id: 6,
-        title: "AQAR REPORT 2018 - 2019",
-        document: AQAR2018,
-    },
-    {
-        id: 7,
-        title: "AQAR REPORT 2019 - 2020",
-        document: AQAR2019,
-    },
-    {
-        id: 8,
-        title: "AQAR REPORT 2020 - 2021",
-        document: AQAR2020,
-    },
-    {
-        id: 9,
-        title: "AQAR REPORT 2021 - 2022",
-        document: AQAR2021,
-    },
-    {
-        id: 10,
-        title: "MEMORANDUM OF UNDERSTANDING",
-        document: MOU,
-    },
-    {
-        id: 11,
-        title: "IQAC MEETING 19 DEC 2022",
-        document: IQACMEETINGS19DEC2022,
-    },
-    {
-        id: 12,
-        title: "IQAC MEETING 10 APRIL 2023",
-        document: IQACMEETINGS10April2023,
-    },
-    {
-        id: 13,
-        title: "Activity 2024",
-        document: Activity,
-    },
-    {
-        id: 14,
-        title: "Annual Report",
-        document: AnnualReport,
-    },
-    {
-        id: 15,
-        title: "IQAC (action taken report)",
-        document: ActiontakenReport,
-    },
-    {
-        id: 16,
-        title: "IQAC members and minutes of meetings",
-        document: MembersAndminutes,
-    },
-    // {
-    //     id: 17,
-    //     title: "MEMORANDUM OF UNDERSTANDING MOU NEW",
-    //     document: MOUPDF,
-    // },
-    {
-        id: 17,
-        title: "SEMESTER WISE CALENDER 21 -23 and 22-24",
-        document: SEMESTERWISECALENDER,
-    },
+  {
+    id: 1,
+    title: "ANNUAL ACADEMIC CALENDAR",
+    document: ACADEMICCALENDAR,
+  },
+  {
+    id: 2,
+    title: "LIST OF IQAC MEETINGS",
+    document: IQACMEETINGS,
+  },
+  {
+    id: 3,
+    title: "LIST OF COMMITTEE MEMBERS",
+    document: CommitteeMembers,
+  },
+  {
+    id: 4,
+    title: "COPY OF STUDENT SATISFACTION SURVEY (SSS)",
+    document: StudentSatisfactionSurvey,
+  },
+  {
+    id: 5,
+    title: "AQAR REPORT 2017 - 2018",
+    document: AQAR2017,
+  },
+  {
+    id: 6,
+    title: "AQAR REPORT 2018 - 2019",
+    document: AQAR2018,
+  },
+  {
+    id: 7,
+    title: "AQAR REPORT 2019 - 2020",
+    document: AQAR2019,
+  },
+  {
+    id: 8,
+    title: "AQAR REPORT 2020 - 2021",
+    document: AQAR2020,
+  },
+  {
+    id: 9,
+    title: "AQAR REPORT 2021 - 2022",
+    document: AQAR2021,
+  },
+  {
+    id: 10,
+    title: "MEMORANDUM OF UNDERSTANDING",
+    document: MOU,
+  },
+  {
+    id: 11,
+    title: "IQAC MEETING 19 DEC 2022",
+    document: IQACMEETINGS19DEC2022,
+  },
+  {
+    id: 12,
+    title: "IQAC MEETING 10 APRIL 2023",
+    document: IQACMEETINGS10April2023,
+  },
+  {
+    id: 13,
+    title: "Activity 2024",
+    document: Activity,
+  },
+  {
+    id: 14,
+    title: "Annual Report",
+    document: AnnualReport,
+  },
+  {
+    id: 15,
+    title: "IQAC (action taken report)",
+    document: ActiontakenReport,
+  },
+  {
+    id: 16,
+    title: "IQAC members and minutes of meetings",
+    document: MembersAndminutes,
+  },
+  // {
+  //     id: 17,
+  //     title: "MEMORANDUM OF UNDERSTANDING MOU NEW",
+  //     document: MOUPDF,
+  // },
+  {
+    id: 17,
+    title: "SEMESTER WISE CALENDER 21 -23 and 22-24",
+    document: SEMESTERWISECALENDER,
+  },
 ];
 
 export const NcteDocument = [
-    {
-        id: 1,
-        title: "NCTE ORDERS 2005",
-        document: NCTE2005,
-    },
-    {
-        id: 2,
-        title: "NCTE ORDERS 2015",
-        document: NCTE2015,
-    },
+  {
+    id: 1,
+    title: "NCTE ORDERS 2005",
+    document: NCTE2005,
+  },
+  {
+    id: 2,
+    title: "NCTE ORDERS 2015",
+    document: NCTE2015,
+  },
+  {
+    id: 3,
+    title: "AFFILIATION ORDERS",
+    document: affilation,
+  },
+  {
+    id: 4,
+    title: "CERTIFIED COPY OF LAND DOCUMENTS",
+    document: land,
+  },
+  {
+    id: 5,
+    title: "LAND USE CERTIFICATE",
+    document: landuse,
+  },
+  {
+    id: 6,
+    title: "BUILDING PLAN APPROVAL & COMPLETION CERTIFICATE",
+    document: buildingplan,
+  },
+  {
+    id: 7,
+    title: "NON-ENCUMBRANCE CERTIFICATE",
+    document: non,
+  },
+  {
+    id: 8,
+    title: "BUILDING SAFETY & DIFFERENTLY ABLED FRIENDLY CERTIFICATE",
+    document: buildingsafety,
+  },
+  {
+    id: 9,
+    title: "FIRE SAFETY CERTIFICATE",
+    document: firesafety,
+  },
+  {
+    id: 10,
+    title: "NAAC CERTIFICATE",
+    document: naacc,
+  },
+  {
+    id: 11,
+    title: "AFFIDAVIT",
+    document: affidavit,
+  },
+  {
+    id: 12,
+    title: "LIBRARY",
+    document: library,
+  },
+  {
+    id: 13,
+    title: "STAFF DETAIL",
+    document: staff,
+  },
+  {
+    id: 14,
+    title: "FEE STRUCTURE",
+    document: fee,
+  },
+  {
+    id: 15,
+    title: "JOINT FDR",
+    document: fdr,
+  },
+  {
+    id: 16,
+    title: "BALANCE SHEET",
+    document: sheet,
+  },
+  {
+    id: 17,
+    title: "INCOME & EXPENDITURE DETAILS",
+    document: income,
+  },
+  {
+    id: 18,
+    title: "RECEIPT AND PAYMENT ACCOUNT FOR THE FINANCIAL YEAR",
+    document: receipt,
+  },
+  {
+    id: 19,
+    title: "Mandatory-Disclosure",
+    document: madatory,
+  },
 ];
